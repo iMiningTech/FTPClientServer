3bf1294a42781726819897db1083b22c27e60575
Blake-IMining <blake@imining.tech>
2023-07-28 11:29:16 +0200

    fixed string issue

--- FTPClient.cpp
+++ FTPClient.cpp
@@ -1,21 +1,19 @@
 #include "FTPClient.h"
 
-FTPClient::FTPClient(FS &_FSImplementation) : FTPCommon(_FSImplementation)
-{
+FTPClient::FTPClient(FS& _FSImplementation) : FTPCommon(_FSImplementation) {
   // set aTimeout to never expire, will be used later by ::waitFor(...)
   aTimeout.resetToNeverExpires();
 }
 
-void FTPClient::begin(const ServerInfo &theServer)
-{
+void FTPClient::begin(const ServerInfo& theServer) {
   _server = &theServer;
 }
 
-const FTPClient::Status &FTPClient::transfer(const String &localFileName, const String &remoteFileName, TransferType direction)
-{
+const FTPClient::Status& FTPClient::transfer(const String& localFileName,
+                                             const String& remoteFileName,
+                                             TransferType direction) {
   _serverStatus.result = PROGRESS;
-  if (ftpState >= cIdle)
-  {
+  if (ftpState >= cIdle) {
     _remoteFileName = remoteFileName;
     _direction = direction;
 
@@ -24,224 +22,167 @@ const FTPClient::Status &FTPClient::transfer(const String &localFileName, const 
     else if (direction & FTP_PUT_NONBLOCKING)
       file = THEFS.open(localFileName, "r");
 
-    if (!file)
-    {
+    if (!file) {
       _serverStatus.result = ERROR;
       _serverStatus.code = errorLocalFile;
       _serverStatus.desc = F("Local file error");
-    }
-    else
-    {
+    } else {
       ftpState = cConnect;
-      if (direction & 0x80)
-      {
-        while (ftpState <= cQuit)
-        {
+      if (direction & 0x80) {
+        while (ftpState <= cQuit) {
           handleFTP();
           delay(25);
         }
       }
     }
-  }
-  else
-  {
+  } else {
     // return error code with status "in PROGRESS"
     _serverStatus.code = errorAlreadyInProgress;
   }
   return _serverStatus;
 }
 
-const FTPClient::Status &FTPClient::check()
-{
+const FTPClient::Status& FTPClient::check() {
   return _serverStatus;
 }
 
-void FTPClient::handleFTP()
-{
-  if (_server == nullptr)
-  {
+void FTPClient::handleFTP() {
+  if (_server == nullptr) {
     _serverStatus.result = TransferResult::ERROR;
     _serverStatus.code = errorUninitialized;
     _serverStatus.desc = F("begin() not called");
-  }
-  else if (ftpState > cIdle)
-  {
+  } else if (ftpState > cIdle) {
     _serverStatus.result = TransferResult::ERROR;
-  }
-  else if (cConnect == ftpState)
-  {
+  } else if (cConnect == ftpState) {
     _serverStatus.code = errorConnectionFailed;
     _serverStatus.desc = F("No connection to FTP server");
-    if (controlConnect())
-    {
-      FTP_DEBUG_MSG("Connection to %s:%u established", control.remoteIP().toString().c_str(), control.remotePort());
+    if (controlConnect()) {
+      FTP_DEBUG_MSG("Connection to %s:%u established",
+                    control.remoteIP().toString().c_str(),
+                    control.remotePort());
       _serverStatus.result = TransferResult::PROGRESS;
       ftpState = cGreet;
-    }
-    else
-    {
+    } else {
       ftpState = cError;
     }
-  }
-  else if (cGreet == ftpState)
-  {
-    if (waitFor(220 /* 220 (vsFTPd version) */, F("No server greeting")))
-    {
+  } else if (cGreet == ftpState) {
+    if (waitFor(220 /* 220 (vsFTPd version) */, F("No server greeting"))) {
       FTP_DEBUG_MSG(">>> USER %s", _server->login.c_str());
       control.printf_P(PSTR("USER %s\n"), _server->login.c_str());
       ftpState = cUser;
     }
-  }
-  else if (cUser == ftpState)
-  {
-    if (waitFor(331 /* 331 Password */))
-    {
+  } else if (cUser == ftpState) {
+    if (waitFor(331 /* 331 Password */)) {
       FTP_DEBUG_MSG(">>> PASS %s", _server->password.c_str());
       control.printf_P(PSTR("PASS %s\n"), _server->password.c_str());
       ftpState = cPassword;
     }
-  }
-  else if (cPassword == ftpState)
-  {
-    if (waitFor(230 /* 230 Login successful*/))
-    {
+  } else if (cPassword == ftpState) {
+    if (waitFor(230 /* 230 Login successful*/)) {
       FTP_DEBUG_MSG(">>> PASV");
       control.printf_P(PSTR("PASV\n"));
       ftpState = cPassive;
     }
-  }
-  else if (cPassive == ftpState)
-  {
-    if (waitFor(227 /* 227 Entering Passive Mode (ip,ip,ip,ip,port,port) */))
-    {
+  } else if (cPassive == ftpState) {
+    if (waitFor(227 /* 227 Entering Passive Mode (ip,ip,ip,ip,port,port) */)) {
       bool parseOK = false;
       // find ()
       uint8_t bracketOpen = _serverStatus.desc.indexOf(F("("));
       uint8_t bracketClose = _serverStatus.desc.indexOf(F(")"));
-      if (bracketOpen && (bracketClose > bracketOpen))
-      {
+      if (bracketOpen && (bracketClose > bracketOpen)) {
         FTP_DEBUG_MSG("Parsing PASV response %s", _serverStatus.desc.c_str());
         _serverStatus.desc[bracketClose] = '\0';
-        if (parseDataIpPort(_serverStatus.desc.c_str() + bracketOpen + 1))
-        {
+        if (parseDataIpPort(_serverStatus.desc.c_str() + bracketOpen + 1)) {
           // catch ip=0.0.0.0 and replace with the control.remoteIP()
-          if (dataIP.toString() == F("0.0.0.0"))
-          {
+          if (dataIP.toString() == F("0.0.0.0")) {
             dataIP = control.remoteIP();
           }
           parseOK = true;
           ftpState = cData;
         }
       }
-      if (!parseOK)
-      {
+      if (!parseOK) {
         _serverStatus.code = errorServerResponse;
         _serverStatus.desc = F("FTP server response not understood.");
       }
     }
-  }
-  else if (cData == ftpState)
-  {
+  } else if (cData == ftpState) {
     // open data connection
-    if (dataConnect() < 0)
-    {
+    if (dataConnect() < 0) {
       _serverStatus.code = errorDataConnectionFailed;
       _serverStatus.desc = F("No data connection to FTP server");
       ftpState = cError;
-    }
-    else
-    {
-      FTP_DEBUG_MSG("Data connection to %s:%u established", data.remoteIP().toString().c_str(), data.remotePort());
+    } else {
+      FTP_DEBUG_MSG("Data connection to %s:%u established",
+                    data.remoteIP().toString().c_str(), data.remotePort());
       millisBeginTrans = millis();
       bytesTransfered = 0;
       ftpState = cTransfer;
-      if (allocateBuffer() == 0)
-      {
+      if (allocateBuffer() == 0) {
         _serverStatus.code = errorMemory;
         _serverStatus.desc = F("No memory for transfer buffer");
         ftpState = cError;
       }
-      if (_direction & FTP_PUT_NONBLOCKING)
-      {
+      if (_direction & FTP_PUT_NONBLOCKING) {
         FTP_DEBUG_MSG(">>> STOR %s", _remoteFileName.c_str());
         control.printf_P(PSTR("STOR %s\n"), _remoteFileName.c_str());
-      }
-      else if (_direction & FTP_GET_NONBLOCKING)
-      {
+      } else if (_direction & FTP_GET_NONBLOCKING) {
         FTP_DEBUG_MSG(">>> RETR %s", _remoteFileName.c_str());
         control.printf_P(PSTR("RETR %s\n"), _remoteFileName.c_str());
       }
     }
-  }
-  else if (cTransfer == ftpState)
-  {
+  } else if (cTransfer == ftpState) {
     bool res = true;
-    if (_direction & FTP_PUT_NONBLOCKING)
-    {
+    if (_direction & FTP_PUT_NONBLOCKING) {
       res = doFiletoNetwork();
-    }
-    else
-    {
+    } else {
       res = doNetworkToFile();
     }
-    if (!res || !data.connected())
-    {
+    if (!res || !data.connected()) {
       ftpState = cFinish;
     }
-  }
-  else if (cFinish == ftpState)
-  {
+  } else if (cFinish == ftpState) {
     closeTransfer();
     ftpState = cQuit;
-  }
-  else if (cQuit == ftpState)
-  {
+  } else if (cQuit == ftpState) {
     FTP_DEBUG_MSG(">>> QUIT");
     control.printf_P(PSTR("QUIT\n"));
     _serverStatus.result = OK;
     ftpState = cIdle;
-  }
-  else if (cIdle == ftpState)
-  {
+  } else if (cIdle == ftpState) {
     stop();
   }
 }
 
-int8_t FTPClient::controlConnect()
-{
-  if (_server->validateCA)
-  {
+int8_t FTPClient::controlConnect() {
+  if (_server->validateCA) {
     FTP_DEBUG_MSG("Ignoring CA verification - FTP only");
   }
   control.connect(_server->servername.c_str(), _server->port);
-  FTP_DEBUG_MSG("Connection to %s:%d ... %s", _server->servername.c_str(), _server->port, control.connected() ? PSTR("OK") : PSTR("failed"));
+  FTP_DEBUG_MSG("Connection to %s:%d ... %s", _server->servername.c_str(),
+                _server->port,
+                control.connected() ? PSTR("OK") : PSTR("failed"));
   if (control.connected())
     return 1;
   return -1;
 }
 
-bool FTPClient::waitFor(const int16_t respCode, const __FlashStringHelper *errorString, uint32_t timeOutMs)
-{
+bool FTPClient::waitFor(const int16_t respCode,
+                        const char* errorString,
+                        uint32_t timeOutMs) {
   // initalize waiting
-  if (!aTimeout.canExpire())
-  {
+  if (!aTimeout.canExpire()) {
     aTimeout.reset(timeOutMs);
     _serverStatus.desc.clear();
-  }
-  else
-  {
+  } else {
     // timeout
-    if (aTimeout.expired())
-    {
+    if (aTimeout.expired()) {
       aTimeout.resetToNeverExpires();
       FTP_DEBUG_MSG("Waiting for code %u - timeout!", respCode);
       _serverStatus.code = errorTimeout;
-      if (errorString)
-      {
+      if (errorString) {
         _serverStatus.desc = errorString;
-      }
-      else
-      {
+      } else {
         _serverStatus.desc = F("timeout");
       }
       ftpState = cTimeout;
@@ -249,12 +190,11 @@ bool FTPClient::waitFor(const int16_t respCode, const __FlashStringHelper *error
     }
 
     // check for bytes from the client
-    while (control.available())
-    {
+    while (control.available()) {
       char c = control.read();
-      //FTP_DEBUG_MSG("readChar() line='%s' <= %c", _serverStatus.desc.c_str(), c);
-      if (c == '\n' || c == '\r')
-      {
+      // FTP_DEBUG_MSG("readChar() line='%s' <= %c", _serverStatus.desc.c_str(),
+      // c);
+      if (c == '\n' || c == '\r') {
         // filter out empty lines
         _serverStatus.desc.trim();
         if (0 == _serverStatus.desc.length())
@@ -262,21 +202,18 @@ bool FTPClient::waitFor(const int16_t respCode, const __FlashStringHelper *error
 
         // line complete, evaluate code
         _serverStatus.code = atoi(_serverStatus.desc.c_str());
-        if (respCode != _serverStatus.code)
-        {
+        if (respCode != _serverStatus.code) {
           ftpState = cError;
-          FTP_DEBUG_MSG("Waiting for code %u but SMTP server replies: %s", respCode, _serverStatus.desc.c_str());
-        }
-        else
-        {
-          FTP_DEBUG_MSG("Waiting for code %u success, SMTP server replies: %s", respCode, _serverStatus.desc.c_str());
+          FTP_DEBUG_MSG("Waiting for code %u but SMTP server replies: %s",
+                        respCode, _serverStatus.desc.c_str());
+        } else {
+          FTP_DEBUG_MSG("Waiting for code %u success, SMTP server replies: %s",
+                        respCode, _serverStatus.desc.c_str());
         }
 
         aTimeout.resetToNeverExpires();
         return (respCode == _serverStatus.code);
-      }
-      else
-      {
+      } else {
         // just add the char
         _serverStatus.desc += c;
       }
